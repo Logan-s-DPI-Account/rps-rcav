5db6cfa8abf41d639fbc3b38e87367aea9011888
Logan CoBell <lcobell@cps.edu>
2022-10-05 17:24:55 +0000

    Scissors logic added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,15 +4,53 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+#Rock Logic
   def play_rock
+     index = rand(3)
+
+moves = ["rock", "paper", "scissors"]
+
+ @comp_move = moves.at(index)
+
+ if @comp_move == "rock" 
+       @outcome = "tied" 
+
+ elsif @comp_move == "scissors"
+  @outcome = "won"
+
+ elsif @comp_move == "paper"
+  @outcome = "lost"
+end
     render({ :template => "game_templates/rock.html.erb"})
     
   end
+
+  #Paper Logic
   def play_paper
+    index = rand(3)
+    
+
     render({ :template => "game_templates/paper.html.erb"})
 
   end 
+
+  #Scissors Logic
   def play_scissors
+     index = rand(3) 
+
+ moves = ["rock", "paper", "scissors"]
+
+ @comp_move = moves.at(index)
+
+ if @comp_move == "scissors" 
+ @outcome = "tied" 
+
+ elsif @comp_move == "rock" 
+ @outcome = "lost" 
+
+ elsif @comp_move == "paper" 
+ @outcome = "won" 
+end
     render({ :template => "game_templates/scissors.html.erb"})
   end
 
